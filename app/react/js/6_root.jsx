c2509e9a97fadd798b7b900f41533caa0c8f8d15
DKBO <dk880842@gmail.com>
2015-08-21 11:55:34 +0800

    Ud

--- app/react/js/6_root.jsx
+++ app/react/js/6_root.jsx
@@ -845,6 +845,29 @@ var Loadbox = React.createClass({
   )
   }
 });
+
+var Disqus = React.createClass({
+componentDidMount: function () {
+      this.addDisqus();
+},
+componentWillUnmount: function () {
+     this.removeDisqus();
+},
+addDisqus: function () {
+      $("body").append("<script id='disqus' type='text/javascript' src='//dkbo-rpg.disqus.com/embed.js' async='true'></script>");
+},
+removeDisqus: function () {
+      $("#disqus").remove();
+},
+render: function () {
+        return (
+          <div>
+            <div id="disqus_thread"></div>
+            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+          </div>
+        );
+    }
+});
 var Load = React.createClass({
   getDefaultProps: function() {
     var x = [];
--- app/react/js/7_menu.jsx
+++ app/react/js/7_menu.jsx
@@ -1,38 +1,3 @@
-init.maps =[
-// Map 0
-  {
-    bg : init.map.bg,
-    col : 1920,
-    row : 1280,
-  in :[{
-      x : 472,
-      y : 544
-    },{
-      x : 1344,
-      y : 512
-    }]
-  },
-// Map 1
-  {
-    bg : init.map.bg,
-    col : 960,
-    row : 640,
-  in :[{
-      x : 472,
-      y : 580
-    }]
-  },
-// Map 2
-  {
-    bg : init.map.bg,
-    col : 960,
-    row : 640,
-  in :[{
-      x : 472,
-      y : 580
-    }]
-  }
-  ]
 init.menuTitle =[ 
     {
       id : 0,
@@ -53,7 +18,7 @@ init.menuTitle =[
     ,
     {
       id : 4,
-      title :"Disq留言"
+      title :"Disqus留言"
     }
   ];
 init.menuText =
