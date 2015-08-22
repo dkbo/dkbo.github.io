57098f71dffa843244e655caf97b1fc7ab1a3afc
DKBO <dk880842@gmail.com>
2015-08-22 08:52:54 +0800

    ud

--- app/react/js/7_menu.jsx
+++ app/react/js/7_menu.jsx
@@ -70,7 +70,7 @@ init.menuText =
     ,
     <aircle className="clearfix">
       <h2>地圖拼圖</h2>
-      <p>來源 : <br /><a href="http://fc03.deviantart.net/fs26/i/2008/149/d/7/rpg_maker_xp_the_best_tileset_by_davide_86.png">fc03.deviantart.net</a></p>
+      <p>來源 : <br /><a href="http://fc03.deviantart.net/fs26/i/2008/149/d/7/rpg_maker_xp_the_best_tileset_by_davide_86.png">www.deviantart.com/</a></p>
       <h2>人物拼圖</h2>
       <p>來源 : <br /><a href="http://doraspace.web.fc2.com/file/image/material/xp/chara/dora_blu.png">doraspace.web.fc2.com</a></p>
     </aircle>
