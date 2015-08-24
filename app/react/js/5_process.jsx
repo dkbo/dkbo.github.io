d899b5ab1e60a27d220dfb49090da2372a77da1c
DKBO <dk880842@gmail.com>
2015-08-24 13:47:01 +0800

    ud

--- app/react/js/5_process.jsx
+++ app/react/js/5_process.jsx
@@ -5,31 +5,12 @@ init.move=[];
 
 //預先載入物件
 for(var j =0;j <init.objects.length;j++){
-  init.events[j]=[];
 init.firstCanvas[j]=[];
 init.secondCanvas[j]=[];
-init.move[j]=[];
-  var xx = 0;
   var yy = 0;
   var zz = 0;
-  var ee = 0;
 for(var i =0;i<init.objects[j].styles.length;i++){
-var obj = init.objects[j].styles[i];
-  if(obj.mx){
-  var x={
-    sx :  obj.mx,
-    sy : obj.my,
-    ex : obj.mw + obj.mx,
-    ey : obj.mh + obj.my,
-    cm : obj.cm,
-    cmm : obj.cmm
-   }
-
-  init.move[j][xx]=x;
-    xx+=1;
-  }
-  if(obj.width){
-    
+var obj = init.objects[j].styles[i];  
 var y={
     left :  obj.left,
     top : obj.top,
@@ -44,11 +25,28 @@ init.firstCanvas[j][yy]=y;
     yy+=1
   }
   else{
-
 init.secondCanvas[j][zz]=y;
   zz+=1
   }
 }
+}
+for(var j =0;j <init.objects.length;j++){
+  init.events[j]=[];
+init.move[j]=[];
+  var xx = 0;
+  var ee = 0;
+for(var i =0;i<init.objects[j].isMove.length;i++){
+var obj = init.objects[j].isMove[i];
+  var x={
+    sx :  obj.mx,
+    sy : obj.my,
+    ex : obj.mw + obj.mx,
+    ey : obj.mh + obj.my,
+    cm : obj.cm,
+    cmm : obj.cmm
+   }
+  init.move[j][xx]=x;
+    xx+=1;
   if(obj.eventID>=0){
     var e={
     sx :  obj.mx,
